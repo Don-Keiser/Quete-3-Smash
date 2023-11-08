9288681a623235a6bed30fc5110da7e1a26f8c91
R0b1n0 <r.hemon@lahorde.school>
2023-11-08 17:10:48 +0100

    assets
    added assets from the asset store, and the first movement script

--- Super Capsule Smash/Assets/script/Input/ScGetInput.cs
+++ Super Capsule Smash/Assets/script/Input/ScGetInput.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ScGetInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private ScPlayerMove movementScript;
+
+    private void Start()
     {
-        
+        movementScript = GetComponent<ScPlayerMove>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void GetLeftJoyStickValue(InputAction.CallbackContext ctxt)
     {
-        
+       movementScript.MoveLeftAndRIght(ctxt.ReadValue<Vector2>().x);
     }
 }
--- Super Capsule Smash/Assets/script/movement/ScPlayerMove.cs
+++ Super Capsule Smash/Assets/script/movement/ScPlayerMove.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class ScPlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float XInput;
+    private bool grounded;
+    private Rigidbody2D rb;
+    private Transform myTransform;
+
+    [SerializeField] private LayerMask ground;
+    [SerializeField] private float MaxXVelocity;
+    [SerializeField] private float dragFactor;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        myTransform = transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        rb.AddForce(new Vector2(XInput, 0) * 100, ForceMode2D.Force);
+        SpeedLimit();
+        if (grounded)
+            ApplyFriction();
+    }
+
+    private void Update()
+    {
+        GroundCheck();
+    }
+
+    public void MoveLeftAndRIght(float movementValue)
+    {
+        XInput = movementValue;
+    }
+
+    private void SpeedLimit()
+    {
+        if (Mathf.Abs(rb.velocity.x) > MaxXVelocity)
+        {
+            rb.velocity = new Vector2(((rb.velocity.x)/ Mathf.Abs(rb.velocity.x))* MaxXVelocity  , rb.velocity.y);
+        }
+    }
+
+    private void GroundCheck()
+    {
+        RaycastHit2D belowMe = Physics2D.Raycast(myTransform.position,Vector2.down,1.1f);
+
+        if (belowMe.collider != null)
+        {
+            if (belowMe.transform.gameObject.layer == 6)
+            {
+                grounded = true;
+            }
+            else 
+                grounded = false;
+        }
+        else
+            grounded = false;
+    }
+
+    private void ApplyFriction()
+    {
+        if (XInput == 0)
+            rb.velocity = new Vector2(rb.velocity.x / dragFactor, rb.velocity.y);
     }
 }
