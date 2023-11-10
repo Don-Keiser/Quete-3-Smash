6f9b5bbd5476250bb7c270bd8be570db54d3f1f7
Don Keiser <sworktale@gmail.com>
2023-11-10 13:58:55 +0100

    Added Damage & Score Systems
    Added Damage and Score Systems on their own, still in debug version for now

---
+++ Super Capsule Smash/Assets/Script/Player/PlayerStats.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int playerScore = 0;
+
+    public int playerDamage = 0;
+}
